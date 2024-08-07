b259cb7a3029450960154fba36a677f4038233b1
thisismelexaaa <yudhaxsinatra@gmail.com>
2024-08-07 22:22:44 +0700

    update

---
+++ public/function_js/konsultasi/index.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Count columns in the table
+    const columns = document.querySelectorAll('.table th').length;
+    const totalColumns = columns - 1;
+    console.log(columns, totalColumns);
+
+    // Initialize DataTable
+    $('#datatable').DataTable({
+        lengthMenu: [
+            [5, 10, 15, 20, -1],
+            [5, 10, 15, 20, 'All']
+        ],
+        dom: 'Blfrtip',
+        // fixedColumns: {
+        //     start: 0,
+        //     end: 1,
+        // },
+        select: true,
+        // scrollCollapse: true,
+        // scrollX: true,
+        pageLength: 5,
+        buttons: [
+            {
+                text: 'Add Data',
+                className: 'btn btn-primary me-2 mb-2',
+                action: () => {
+                    const customersRouteCreate = "account/create";
+                    window.location.href = customersRouteCreate;
+                }
+            },
+            {
+                extend: 'collection',
+                text: 'Export',
+                className: 'btn btn-primary me-2 mb-2',
+                buttons: [
+                    {
+                        extend: 'copy',
+                        title: `Data customers Metamorfosa, tanggal ${new Date().toLocaleDateString()}`,
+                        exportOptions: {
+                            columns: [...Array(totalColumns).keys()],
+                            modifier: {
+                                page: 'current'
+                            }
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        text: 'XLSX',
+                        title: `Data customers Metamorfosa, tanggal ${new Date().toLocaleDateString()}`,
+                        exportOptions: {
+                            columns: [...Array(totalColumns).keys()],
+                            modifier: {
+                                page: 'current'
+                            }
+                        }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        text: 'PDF',
+                        title: `Data customers Metamorfosa, tanggal ${new Date().toLocaleDateString()}`,
+                        exportOptions: {
+                            columns: [...Array(totalColumns).keys()],
+                            modifier: {
+                                page: 'current'
+                            }
+                        },
+                        orientation: 'landscape',
+                        pageSize: 'A4'
+                        // customize: function(doc) {
+                        //     doc.pageMargins = [10, 10, 10,
+                        //     10]; // Optional: Customize page margins
+                        // }
+                    },
+                    {
+                        extend: 'csv',
+                        text: 'CSV',
+                        title: `Data customers Metamorfosa, tanggal ${new Date().toLocaleDateString()}`,
+                        exportOptions: {
+                            columns: [...Array(totalColumns).keys()],
+                            modifier: {
+                                page: 'current'
+                            }
+                        }
+                    },
+                ]
+            },
+        ],
+    });
+});
--- resources/views/panel/konsultasi/index.blade.php
+++ resources/views/panel/konsultasi/index.blade.php
@@ -28,11 +28,55 @@
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
-                <h1>Kerjaannya zahrin</h1>
+                <div class="table-responsive">
+                    <table class="table table-hover" id="datatable">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nama Pelanggan</th>
+                                <th>Layanan</th>
+                                <th>Profesional</th>
+                                <th>Keluhan</th>
+                                <th>Hasil Konsultasi</th>
+                                <th>Tanggal Masuk</th>
+                                <th>Tanggal Selesai</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @for ($i = 0; $i < 10; $i++)
+                                <tr>
+                                    <td>{{ $i }}</td>
+                                    <td>John</td>
+                                    <td>Psikologi</td>
+                                    <td>Support Teacher {{ $i }}</td>
+                                    <td>Kurang Bisa Berbicara</td>
+                                    <td>Baik</td>
+                                    <td>01/01/2024</td>
+                                    <td>01/02/2024</td>
+                                    <td>
+                                        <div class="d-flex gap-2">
+                                            <a href="#" class="btn btn-sm btn-primary">
+                                                <i class="bi bi-info-circle-fill"></i>
+                                            </a>
+                                            <a href="#" class="btn btn-sm btn-warning">
+                                                <i class="bi bi-pencil-square"></i>
+                                            </a>
+                                            <a href="#" class="btn btn-sm btn-danger">
+                                                <i class="bi bi-trash3-fill"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endfor
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 @endsection
 
 @section('scripts')
+    <script src="{{ asset('function_js/konsultasi/index.js') }}"></script>
 @endsection
